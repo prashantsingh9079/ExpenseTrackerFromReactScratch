976b834cc86fedd04665697d5cff64a129322f99
prashantsingh9079 <prashant.singh9079@gmail.com>
2023-06-09 06:04:05 +0530

    props used and date converted

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 const App = () => {
+  const expenses = [{ title: "Car Insurance", amount: "8200", date: new Date(2023, 5, 8) },
+  { title: "Petrol", amount: "3200", date: new Date(2023, 5, 9) },
+  { title: "Shopping", amount: "6900", date: new Date(2023, 5, 9) }];
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem/>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,20 @@
 import './ExpenseItem.css';
 
 
-const ExpenseItem = () => {
-
-    const ExpenseDate = new Date(2023,5,8);
-    const ExpenseTitle = 'Car Insurance';
-    const ExpenseAmount = 8200;
-
+const ExpenseItem = (props) => {
+    const month = props.date.toLocaleString('en-IN',{month:'long'});
+    const day = props.date.toLocaleString('en-IN',{day:'2-digit'});
+    const year = props.date.getFullYear();
     return (
         <div className="expense-item">
-            <div>{ExpenseDate.toISOString()}</div>
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
             <div className="expense-item__description">
-                <h2>{ExpenseTitle}</h2>
-                <div className="expense-item__price">Rs{ExpenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">Rs {props.amount}</div>
             </div>
         </div>
     )
